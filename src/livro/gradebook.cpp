881d6802d5fe7ffdfcfdfc086d75b203cd9a69b0
lunhg <lunhanig@gmail.com>
2021-03-10 22:48:56 -0300

    adicionado método 'determineClassAverage' em gradebook

--- src/livro/gradebook.cpp
+++ src/livro/gradebook.cpp
@@ -12,7 +12,12 @@ GradeBook::GradeBook(string name) {
 
 // função para configurar o nome do curso
 void GradeBook::setCourseName(string name) {
-    courseName = name;
+    if ( name.length() <= 25) {
+        courseName = name;
+    } else {
+        courseName = name.substr(0,25);
+        cout << "Nome \"" << name << "\" excede o tamanho máximo (25).\n" << endl;
+    }
 }
 
 // funcção para obter o nome do curso
@@ -20,9 +25,28 @@ string GradeBook::getCourseName() {
     return courseName;
 }
 
-
 // exibe uma mensagem de boas-vindas
 void GradeBook::displayMessage() {
     cout << "Welcome to the gradebook v" << UNIVESP_COM_160_VERSION_MAJOR << "." << UNIVESP_COM_160_VERSION_MINOR << endl;
     cout << "  " << getCourseName() << endl;
 }
+
+void GradeBook::determineClassAverage() {
+    int total;
+    int gradeCounter;
+    int grade;
+    int average;
+    total = 0;
+    gradeCounter = 1;
+    while(gradeCounter <= 10) {
+        cout << "Enter grade: ";
+        std::cin >> grade;
+        total = total + grade;
+        gradeCounter = gradeCounter + 1;
+    }
+    average = total / 10;
+    cout  << "\nTotal: " << total << endl;
+    cout << "Média:" << average << endl;
+}
+
+
--- src/livro/gradebook_main.cpp
+++ src/livro/gradebook_main.cpp
@@ -6,13 +6,7 @@ using std::endl;
 
 int main() {
     GradeBook gradebook1("CS101 Introdução à programação C++");
-    GradeBook gradebook2("CS102 Estruturas de dados em C++");
-
-    cout << "gradebook1 criado para o curso: " << gradebook1.getCourseName() << endl;
-    cout << "" << endl;
-    cout << "gradebook2 criado para o curso: " << gradebook2.getCourseName() << endl;
-    cout << "" << endl;
     gradebook1.displayMessage();
-    gradebook2.displayMessage();
+    gradebook1.determineClassAverage();
     return 0;
 }
